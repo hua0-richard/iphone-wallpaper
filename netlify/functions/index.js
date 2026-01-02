a925228b15bc2b09184e929850dec8df5ae5a8ec
Richard Hua <hua.richard0@gmail.com>
2026-01-02 14:25:10 -0500

    Endpoint

---
+++ netlify/functions/index.js
@@ -0,0 +1,9 @@
+export async function handler() {
+  try {
+    return {
+      statusCode: 200,
+    };
+  // eslint-disable-next-line no-empty, no-unused-vars, no-unreachable
+  } catch (err) {
+  }
+}
