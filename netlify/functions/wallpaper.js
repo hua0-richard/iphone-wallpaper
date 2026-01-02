9253a7519f1ee0323fe1895ab901debcb1028dda
Richard Hua <hua.richard0@gmail.com>
2026-01-02 14:46:26 -0500

    Fix

--- netlify/functions/wallpaper.js
+++ netlify/functions/wallpaper.js
@@ -1,9 +1,9 @@
 import { createCanvas, GlobalFonts } from '@napi-rs/canvas';
 import path from 'path';
 import { fileURLToPath } from 'url';
-
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+
 GlobalFonts.registerFromPath(
   path.join(__dirname, 'fonts/JetBrainsMono.ttf'),
   'Inter'
