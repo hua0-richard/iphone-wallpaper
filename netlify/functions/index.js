4cd0571ee0cdb21eafeb3f3249f2d8597a5ea375
Richard Hua <hua.richard0@gmail.com>
2026-01-02 18:32:48 -0500

    Boilerplate

--- netlify/functions/index.js
+++ netlify/functions/index.js
@@ -1,9 +1,5 @@
 export async function handler() {
-  try {
-    return {
-      statusCode: 200,
-    };
-  // eslint-disable-next-line no-empty, no-unused-vars, no-unreachable
-  } catch (err) {
-  }
+  return {
+    
+  } 
 }
